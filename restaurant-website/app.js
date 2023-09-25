48cb25fc7687ff04cfa95b713862f9394425c469
Omor Faruk <130866014+omor777@users.noreply.github.com>
2023-09-25 16:27:20 +0600

    Restaurant_Web

---
+++ restaurant-website/app.js
@@ -0,0 +1,8 @@
+// const formInput = document.querySelectorAll("input");
+// console.log(formInput);
+// formInput.forEach((input) => {
+//   input.addEventListener("focus", function (e) {
+//     let target = e.target;
+//     target.parentElement.style.border = "1px solid red";
+//   });
+// });
